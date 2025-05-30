caafcee54e6a98c5259f6d84ccee5b9471403119
Thang <vvthang.dev@gmail.com>
2025-05-30 20:50:55 +0700

    v1.1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/user/", // Đặt base URL là /user/
   server: {
-    port: 3100,
+    host: "0.0.0.0", // Bind tới tất cả giao diện mạng
+    port: 3100, // Đảm bảo cổng đúng
   },
-  base: '/user/', // Đặt base URL là /user/
 });
