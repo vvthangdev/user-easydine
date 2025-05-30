0034d2577a0a22c14b16719ccc52712b231a1355
Thang <vvthang.dev@gmail.com>
2025-05-31 00:30:31 +0700

    ok 1

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/user/",
+  base: "/user/", // dùng để xử lý routing chính xác khi truy cập ở /user/
   server: {
     host: "0.0.0.0",
     port: 3100,
-    allowedHosts: ["vuvanthang.website", "localhost"], // THÊM host rõ ràng
+    allowedHosts: [
+      "vuvanthang.website",
+      "128.199.246.55",  // THÊM IP PUBLIC
+      "localhost",
+      "127.0.0.1"
+    ],
     strictPort: true,
-    cors: true,
+    cors: true
   },
 });
